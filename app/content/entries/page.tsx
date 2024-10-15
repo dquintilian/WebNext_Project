d47e085af0882e6de039e3b9568d4eb3a20fb349
Dominic Quintilian <dquintilian@gmail.com>
2024-10-15 10:24:31 -0400

    Moving directories around

--- app/content/entries/page.tsx
+++ app/content/entries/page.tsx
@@ -1,18 +1,19 @@
 // app/page.tsx
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS, MARKS, Document } from '@contentful/rich-text-types';
-import { fetchEntries } from '../../../utils/articleList'; // Import a function to fetch a list of article entries, adjust the path as needed
-import { Entry, EntryCollection, EntrySkeletonType } from 'contentful';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link'
-import { Span } from 'next/dist/trace';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, MARKS, Document } from "@contentful/rich-text-types";
+import { fetchEntries } from "../../../lib/utils/articleList"; // Import a function to fetch a list of article entries, adjust the path as needed
+import { Entry, EntryCollection, EntrySkeletonType } from "contentful";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { Span } from "next/dist/trace";
 
 // Define types for your content (this is useful for TypeScript)
 
 export default async function HomePage() {
   // Fetch entries from Contentful asynchronously using fetchEntries function
   // Note: Fix any issues with fields later if needed
-  const entries: EntryCollection<EntrySkeletonType> | null = await fetchEntries();
+  const entries: EntryCollection<EntrySkeletonType> | null =
+    await fetchEntries();
 
   // If no entries are found or the entries are not in the expected array format, display a fallback message
   if (!entries || !Array.isArray(entries.items)) {
@@ -23,13 +24,19 @@ export default async function HomePage() {
   const options = {
     renderMark: {
       // Render bold text using a <strong> element with a custom class
-      [MARKS.BOLD]: (text: string) => <strong className='text-bold'>{text}</strong>,
+      [MARKS.BOLD]: (text: string) => (
+        <strong className="text-bold">{text}</strong>
+      ),
     },
     renderNode: {
       // Render <h3> elements with custom styling
-      [BLOCKS.HEADING_3]: (node: any, children: any) => <h3 className='text-2xl py-4'>subTitle:{children}</h3>,
+      [BLOCKS.HEADING_3]: (node: any, children: any) => (
+        <h3 className="text-2xl py-4">subTitle:{children}</h3>
+      ),
       // Render paragraphs with custom styling
-      [BLOCKS.PARAGRAPH]: (node: any, children: any) => <p className='py-4'>Paragraph:{children}</p>,
+      [BLOCKS.PARAGRAPH]: (node: any, children: any) => (
+        <p className="py-4">Paragraph:{children}</p>
+      ),
     },
   };
 
@@ -38,12 +45,9 @@ export default async function HomePage() {
     <div className="max-w-4xl mx-auto px-4">
       {/* Iterate through the list of entries and render each one */}
       <div>
-        <span>
-          {/* <BlogFilter /> */}
-        </span>
+        <span>{/* <BlogFilter /> */}</span>
       </div>
       {entries.items.map((item) => (
-    
         <div
           key={item.sys.id}
           className="my-10 p-6 border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
@@ -51,7 +55,10 @@ export default async function HomePage() {
           {/* Render the article title or a default value if it's not available */}
           <h2 className="text-2xl font-semibold py-4 text-white">
             <Link href={`/content/entries/${item.sys.id}`}>
-              {typeof item.fields.articleTitle === 'string' ? item.fields.articleTitle : 'Untitled Article'}.
+              {typeof item.fields.articleTitle === "string"
+                ? item.fields.articleTitle
+                : "Untitled Article"}
+              .
             </Link>
           </h2>
         </div>
--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,14 +1,14 @@
-import { fetchEntry } from './../../utils/articleLoader'; // Import a function to fetch an article entry, adjust the path to match your project structure
+import { fetchEntry } from "../../lib/utils/articleLoader"; // Import a function to fetch an article entry, adjust the path to match your project structure
 
 export default async function Page() {
   // Fetch the article entry asynchronously using fetchEntry function
   const entry = await fetchEntry();
 
   // Log the entire entry to inspect its structure and understand the data
-  console.log('Fetched entry:', entry?.fields?.articleBody?.content);
+  console.log("Fetched entry:", entry?.fields?.articleBody?.content);
 
   // Extract the article title from the fetched entry, or use a default value if not available
-  const articleTitle = entry?.fields?.articleTitle || 'No title available';
+  const articleTitle = entry?.fields?.articleTitle || "No title available";
   // Extract the article body content from the fetched entry, or set it as an empty array if not available
   const articleBody = entry?.fields?.articleBody?.content || [];
 
@@ -22,7 +22,7 @@ export default async function Page() {
         {articleBody.map((paragraph: any, index: number) => (
           <p key={index}>
             {/* Iterate through the content of each paragraph and join the text nodes */}
-            {paragraph.content.map((textNode: any) => textNode.value).join(' ')}
+            {paragraph.content.map((textNode: any) => textNode.value).join(" ")}
           </p>
         ))}
       </div>
