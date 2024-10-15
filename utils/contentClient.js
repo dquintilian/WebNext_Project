6b0904f63e82cfd54435ba261e8eadcb505f8580
Dominic Quintilian <dquintilian@gmail.com>
2024-10-15 10:22:40 -0400

    modification of env variables

--- utils/contentClient.js
+++ utils/contentClient.js
@@ -1,11 +1,11 @@
 const contentful = require('contentful');
-
+require('dotenv').config();
 
 // Create the authorization for contentful
 
 export const client = contentful.createClient({
-  space: 'h02wmcwkik29',
-  environment: 'master',
-  accessToken: '8PMsoE3EtqxriO3oUJcPFdPGb_EMYAfvLfGU632B44s',
+  space: process.env.SPACE,
+  environment: process.env.ENVIRONMENT,
+  accessToken: process.env.ACCESS_TOKEN,
 });
 
